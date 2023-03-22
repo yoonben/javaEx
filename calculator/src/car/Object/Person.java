baa21c1d4c25c08fca44879b6a63eb4a00f32b69
user <user@DESKTOP-2PGRMF4>
2023-03-22 11:32:47 +0900

    2주차

--- calculator/src/car/Object/Person.java
+++ calculator/src/car/Object/Person.java
@@ -5,7 +5,7 @@ public class Person {
 	public static void main(String[] args) {
 		
 		
-		Car car = new Car();
+		/*Car car = new Car();
 		Car carGia = new Car("기아자동차");
 		
 		//필드에 접근하기 위해서는 객체가 생성되어 있어야 한다.
@@ -13,7 +13,18 @@ public class Person {
 		car.speedup();
 		System.err.println("car.speed: "+car.speed);
 		
-		System.err.println("cargia.company : "+carGia.company);
+		System.err.println("cargia.company : "+carGia.company);*/
+		
+		Car car = new Car();
+		car.company = "르노삼성";
+		System.out.println("company: "+car.company);
+		System.out.println("speed: "+car.speed);
+		car.speedup();
+		System.out.println("speed: "+car.speed);
+		car.speed = 100;
+		System.out.println("car.speed = 100");
+		System.out.println("speed: "+car.speed);
+		
 	}
 
 }
